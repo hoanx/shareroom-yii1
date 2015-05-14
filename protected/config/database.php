5e562e607d64b18bb551ac029cb77147f31d5a08
unknown <xuanhoapro@gmail.com>
2015-05-14 08:45:04 +0700

    config layout first

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,13 +2,11 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+//	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+	'connectionString' => 'mysql:host=localhost;dbname=shareroom-yii1',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '',
+	'password' => '123456a@',
 	'charset' => 'utf8',
-	*/
 );
--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Share Room',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -20,14 +20,12 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'1',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
 	),
 
 	// application components
@@ -39,16 +37,17 @@ return array(
 		),
 
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+            'showScriptName' => false,
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
+                '<module:\w+>/<controller:\w+>/<action:\w+>'=>'<module>/<controller>/<action>',
+            ),
 		),
-		*/
+
 
 		// database settings are configured in database.php
 		'db'=>require(dirname(__FILE__).'/database.php'),
@@ -66,13 +65,21 @@ return array(
 					'levels'=>'error, warning',
 				),
 				// uncomment the following to show log messages on web pages
-				/*
-				array(
+				/*array(
 					'class'=>'CWebLogRoute',
-				),
-				*/
+				),*/
 			),
 		),
+
+        'clientScript' => array(
+            'class' => 'MyClientScript',
+            'scriptMap' => array(
+                'jquery.js'=>false,  //disable default implementation of jquery
+                'jquery.min.js'=>false,  //desable any others default implementation
+                'jquery.ba-bbq.js' => false,
+//                 'jquery.yiigridview.js' => false,
+            )
+        ),
 
 	),
 
@@ -80,6 +87,6 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'xuanhoapro@gmail.com',
 	),
 );
