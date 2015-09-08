c3dd759a4207b2efb91da9429c36b787645c6a03
unknown <xuanhoapro@gmail.com>
2015-09-08 19:58:43 +0700

    add view

---
+++ protected/views/site/about.php
@@ -0,0 +1,39 @@
+<div class="page-static">
+    <div class="row">
+        <div class="col-md-8">
+            <h2><?php echo($this->getPageTitle()) ?></h2>
+            <hr>
+            <p>Với sứ mệnh đem đến trải nghiệm du lịch độc đáo cho khách hàng, Công ty cổ phần Shareroom chuyên cung cấp
+                dịch vụ đặt phòng homestayvới mức giá hợp lý, giúp bạn chọn lựa và lên kế hoạch cho chuyến du lịch của
+                mình.</p>
+
+            <p>Với hệ thống đặt phòng trực tuyền tại website: <a href="http://shareroom.vn/">http://shareroom.vn/</a>
+                cùng
+                các chức năng hỗ trợ khách hàng lựa
+                chọn như: Tìm kiếm phòng trống, Tìm phòng có giá rẻ, Tìm kiếm theo địa điểm và đội ngũ nhân viên Hỗ trợ
+                đặt
+                phòng trực tuyến sẽ giúp khách hàng đặt phòng dễ dàng hơn bao giờ hết.</p>
+
+        </div>
+        <div class="col-md-4">
+            <?php echo(CHtml::image(Yii::app()->baseUrl . '/images/about.png', '', array('class' => 'img-responsive'))) ?>
+        </div>
+    </div>
+    <div class="row">
+        <h2>Về chúng tôi</h2>
+        <hr>
+        <p>Được xây dựng từ tháng 6 năm 2015, Shareroom ra đời với mục đích kết nối khách du lịch với người dân địa
+            phương
+            có phòng trống cho thuê tại các địa điểm du lịch trong cả nước. </p>
+
+        <p>Bằng cách tận dụng phòng trống trong hộ gia đình, nhà riêng chưa sử dụng shareroom giúp giảm chi phí đặt
+            phòng
+            khi đi du lịch. Với 3 loại phòng: phòng ở ghép, phòng riêng, cả căn hộ. Tùy theo nhu cầu bạn có thể chọn
+            phòng
+            phù hợp với chuyến du lịch </p>
+
+        <div class="author">
+            <h3>Người sáng lập</h3>
+        </div>
+    </div>
+</div>
