c6ff2d7cbdedbb29990ad1129742bb487efd2093
HoaNguyen <HoaNguyen@HoaNguyens-MacBook-Pro.local>
2015-09-12 00:16:21 +0700

    template mail new pass for admin

---
+++ protected/views/template/password_for_manager.php
@@ -0,0 +1,22 @@
+<div bgcolor="#FFFFFF" style="margin:0;padding:0;font-family:'Helvetica Neue','Helvetica',Arial,sans-serif;min-height:100%;width:100%">
+
+<table style="width: 100% !important" cellpadding="0" cellspacing="0">
+	<tbody>
+		<tr>
+			<td style="margin: 0; padding: 0">&nbsp;</td>
+			<td style="margin: 0 auto; padding: 0; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; display: block; max-width: 600px; clear: both">
+				<div style="margin: 0 auto; padding: 15px; font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; max-width: 600px; display: block">
+					<h3 style="margin: 0 0 15px 0; padding: 0; font-family: 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; line-height: 1.1; color: #111111; font-weight: 500; font-size: 27px">
+					    Xin chào <?php echo $model->username ?>
+					</h3>
+					<div style="margin: 0; padding: 10px; font-family: 'Helvetica Neue', 'Helvetica', Arial, sans-serif; font-weight: normal; font-size: 14px; line-height: 1.6; background-color: #ebebeb; color: #111111">
+						Mật khẩu hiện tại của bạn là: <b><?php echo $model->password; ?></b>
+					</div>
+					<br> <br>
+				</div>
+
+			</td>
+			<td style="margin: 0; padding: 0">&nbsp;</td>
+		</tr>
+	</tbody>
+</table>
